a50ae44b76c8786a5d236b2e6c899d810142237e
steven.lin <steven.lin@bytedance.com>
2023-05-13 01:04:21 -0700

    backend add popular counter

--- app/api/[shortUrl]/route.js
+++ app/api/[shortUrl]/route.js
@@ -2,11 +2,15 @@ import client from "@/utils/redisClient"
 
 export const GET = async (request, { params }) => {
     try {
-        console.log(params)
+        console.log("params:", params.shortUrl)
         rsp = await client.hGetAll("shortUrl:" + params.shortUrl)
         console.log({ rsp })
-        return new Response(JSON.stringify(rsp), { status: 200 })
+        if (rsp != null) {
+            console.log(params.shortUrl)
+            await client.zIncrBy("popular", 1, params.shortUrl)
+        }
+        return new Response(rsp.longUrl, { status: 200 })
     } catch (error) {
-        return new Response("Internal Server Error", { status: 500 });
+        return new Response("Internal Server Error " + error, { status: 500 });
     }
 }
---
+++ app/api/search/popular/route.js
@@ -0,0 +1,12 @@
+import client from "@/utils/redisClient"
+
+export const GET = async (request) => {
+    try {
+
+        rsp = await client.zRange("popular", 0, 19)
+        console.log({ rsp })
+        return new Response(rsp, { status: 200 })
+    } catch (error) {
+        return new Response("Internal Server Error " + error, { status: 500 });
+    }
+}
