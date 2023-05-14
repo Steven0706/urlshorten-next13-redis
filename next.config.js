28855700fa1c7f539450e293987450e0412ea7d4
steven.lin <steven.lin@bytedance.com>
2023-05-14 01:05:59 -0700

    remove docker compose

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack(config) {
-        config.experiments = {
-            ...config.experiments,
-            topLevelAwait: true,
-        }
-        return config
-    }
+
 }
 
 module.exports = nextConfig
--- utils/redisClient.ts
+++ utils/redisClient.ts
@@ -3,7 +3,7 @@ import * as redis from 'redis';
 import { SchemaFieldTypes } from 'redis';
 
 const redisUrl = 'redis://localhost:6379';
-const client = redis.createClient({ url: process.env.redisUrl });
+const client = redis.createClient({ url: redisUrl });
 
 client.on('connect', () => {
     console.log('Redis connected');
